3734df930bbfc1015cac89dec01a6c4827a2fb37
fgeanlopes <f.geanlopes@gmail.com>
2019-03-31 13:18:30 -0300

    update do projeto V 1.0

---
+++ README.md
@@ -0,0 +1,3 @@
+1 - Para usar basta dar npm install
+
+2 - Adicionar o caminho do projeto no arquivo "gulfile.js -> na função:reset_heroi: Linha : 57
---
+++ source/php/index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!--BASE SEO-->
+    <meta name="robots" content="follow , index"/>
+    <meta name="description" content=""/>
+
+    <title>Nome do Projeto</title>
+
+    <link rel="stylesheet" type="text/css" media="all" href="dist/css/style.css"/>
+
+</head>
+<body>
+
